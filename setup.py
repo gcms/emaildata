203a4e7eaadab5e744601541f4d0616b764dad55
Karel Antonio Verdecia Ortiz <kverdecia@gmail.com>
2015-03-15 23:15:05 -0300

    - Updated setup.py. - Added support for metadata extraction.

--- setup.py
+++ setup.py
@@ -1,26 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+This file is part of the emailcontent package
+Copyrighted by Karel Vedecia Ortiz <kverdecia@gmail.com>
+License: LGPLv3 (http://www.gnu.org/licenses/lgpl.html)
+"""
+__author__ = "Karel Antonio Verdecia Ortiz"
+__contact__ = "kverdecia@gmail.com"
+
+
 from setuptools import setup, find_packages
 import sys, os
 
-version = '0.0'
+
+version = '0.1'
+
 
 setup(name='emaildata',
-      version=version,
-      description="Python package for extracting metadata, text, html and attachements from email messages.",
-      long_description="""\
+    version=version,
+    description="Python package for extracting metadata, text, html and attachements from email messages.",
+    long_description="""\
 """,
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='email rfc822',
-      author='Karel Antonio Verdecia Ortiz',
-      author_email='kverdecia@gmail.com',
-      url='',
-      license='LPGL3',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=True,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+    classifiers=[
+        "Topic :: Communications :: Email",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: Text Processing",
+    ],
+    keywords='email rfc822',
+    author='Karel Antonio Verdecia Ortiz',
+    author_email='kverdecia@gmail.com',
+    url='',
+    license='LPGL3',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    include_package_data=True,
+    zip_safe=True,
+    install_requires=[
+        'charset>=1.0.1',
+    ],
+    entry_points="""
+    # -*- Entry points: -*-
+    """,
+)
